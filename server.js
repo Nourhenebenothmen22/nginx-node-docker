cd722edf43cd71d72d1ae70e40660808c6d7345c
nourhene <benothmennourhen8@gmail.com>
2025-12-16 20:40:10 +0100

    📝 Update server.js to support APP_NAME environment variable for Docker Compose

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// Récupérer le nom de l'app depuis Docker Compose
+const appName = process.env.APP_NAME || 'Node App';
+
 // Servir le dossier images
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
@@ -12,7 +15,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Ajouter un endpoint pour identifier l'app
+app.get('/info', (req, res) => {
+  res.json({ app: appName, message: "Hello from Node.js DevOps app!" });
+});
+
 // Lancer le serveur
 app.listen(port, () => {
-  console.log(`Serveur lancé sur http://localhost:${port}`);
+  console.log(`${appName} lancé sur http://localhost:${port}`);
 });
